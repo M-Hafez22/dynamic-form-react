21c602291f17bac4480926b6494dbdc7c8400eb7
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-25 08:57:58 +0200

    Display user input

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from "react"
+import { UserInputContext } from "../contexts/userInput"
 
 function Result() {
+  const [{ formItems }, setFormItems] = useContext(UserInputContext)
+  const feildsOut = formItems.map(
+    item =>
+      item.name && (
+        <p key={item.name}>
+          {item.name}: <span>{item?.value}</span>
+        </p>
+      )
+  )
   return (
-    <div>Result</div>
+    <div className="userinfo">
+      <h1>UserInfo</h1>
+      <div>{feildsOut}</div>
+    </div>
   )
 }
 
