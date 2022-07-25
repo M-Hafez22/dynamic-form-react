66db17463d81e2167b7b63c42fbd8a67d667f601
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-25 09:02:50 +0200

    💄 Add styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { UserInputProvider } from "./contexts/userInput"
 import { ThemeProvider } from "./contexts/theme"
+import "./assets/main.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
