1d18cae42dc7c7ecd16b84beb34c823620ae520b
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-25 08:10:17 +0200

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from "react"
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from './components/Home'
+import Result from './components/Result'
 
 function App() {
-  return <div className="App">Dynamic Form</div>
+  return (
+    <div className="App">
+      <Router>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/result">Result</Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
