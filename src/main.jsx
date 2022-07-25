d06e740eb1ac4751ecc86133658e2cfd250823ac
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-25 08:30:22 +0200

    Add UserInputProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { UserInputProvider } from "./contexts/userInput"
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <UserInputProvider>
+      <App />
+    </UserInputProvider>
   </React.StrictMode>
 )
