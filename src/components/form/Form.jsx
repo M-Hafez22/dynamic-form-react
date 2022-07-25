e7a1d069f3f2046e67ac37dd857f0bbb74fb0398
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-25 09:19:31 +0200

    💄 display file input with the default style

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,15 +4,14 @@ import { UserInputContext } from "../../contexts/userInput"
 
 export default function Form() {
   const [{ formItems }, setFormItems] = useContext(UserInputContext)
-  const feilds = formItems.map(
-    (item, i) =>
+  const feilds = formItems.map((item, i) =>
+    item.type === "file" ? (
+      <input type={item.type} key={item.name} required />
+    ) : (
       item.type &&
       item.name && <Feild type={item.type} key={item.name} text={item.name} />
+    )
   )
 
-  return (
-    <form className="form">
-      {feilds}
-    </form>
-  )
+  return <form className="form">{feilds}</form>
 }
